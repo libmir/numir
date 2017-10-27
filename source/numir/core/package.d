f160b929a980a1bbcd2c95274f0799705072f7e1
jmh530 <jmh530@users.noreply.github.com>
2017-10-27 14:39:08 -0400

    Revise package.d with most important UTs

--- source/numir/core/package.d
+++ source/numir/core/package.d
@@ -64,23 +64,16 @@ unittest
     assert(e1.shape == e0.shape);
     e0[0, 0] += 1;
     assert(e1 != e0);
-    alias E0 = DeepElementType!(typeof(e0));
-    alias E1 = DeepElementType!(typeof(e1));
-    static assert(is(E0 == E1));
 
     // np.ones, ones_like
     auto o = ones(2, 3);
     assert(o.all!(x => x == 1));
     assert(o.shape == [2, 3]);
-    assert(o == ones([2, 3]));
-    assert(o == o.ones_like);
 
     // np.zeros, np.zeros_like
     auto z = zeros(2, 3);
     assert(z.all!(x => x == 0));
     assert(z.shape == [2, 3]);
-    assert(z == zeros([2, 3]));
-    assert(z == z.zeros_like);
 
     // np.eye, identity
     assert(eye(2, 3, 1) == [[0.0, 1.0, 0.0],
@@ -103,33 +96,18 @@ unittest
        np.concatenate            | concatenate
     */
 
-    import mir.ndslice.slice : sliced, DeepElementType;
-    import mir.ndslice.topology : iota, universal;
-    import mir.ndslice.dynamic : transposed;
+    import mir.ndslice.slice : sliced;
 
-    auto s = [[1,2],[3,4]].sliced; // mir's sliced
+    auto s = [[1, 2],[3, 4]].sliced; // mir's sliced
     // error: s[0, 0] = -1;
 
-    auto m = nparray([[1,2],[3,4]]);
+    auto m = nparray([[1, 2],[3, 4]]);
     m[0, 0] = -1;
-    assert(m == [[-1,2], [3,4]]);
-    static assert(is(DeepElementType!(typeof(m)) == int)); // maybe double?
-
-    auto v = nparray([1, 2]);
-    v[1] = -2;
-    assert(v == [1, -2]);
+    assert(m == [[-1, 2], [3, 4]]);
 
     auto u = nparray([[5, 6]]);
     assert(concatenate(m, u) == [[-1, 2], [3, 4], [5, 6]]);
     assert(concatenate(u, m) == [[5, 6], [-1, 2], [3, 4]]);
-
-    auto uT = u.universal.transposed;
-    assert(concatenate!1(m, uT) == [[-1, 2, 5], [3, 4, 6]]);
-
-    assert(concatenate!0([[0,1]].nparray, [[2,3]].nparray, [[4,5]].nparray) == iota(3, 2));
-    // axis=-1 is the same to axis=$-1
-    assert(concatenate!(-1)([[0,1]].nparray, [[2,3]].nparray, [[4,5]].nparray) == [iota(6)]);
-    assert(concatenate!(-1)([[0,1]].nparray, [[2]].nparray) == [[0, 1, 2]]);
 }
 
 ///
@@ -172,8 +150,8 @@ unittest
     //  -------
     auto a = iota(2, 3);
     assert(a.diag == [0, 4]);
-    assert(a.diag!(1)(1) == [1, 5]);
-    assert(a.diag!(0)(1) == [3]);
+    assert(a.diag!1(1) == [1, 5]);
+    assert(a.diag!0(1) == [3]);
 }
 
 ///
@@ -181,11 +159,14 @@ unittest
 {
     import mir.ndslice.topology : iota;
     import mir.ndslice.slice : sliced;
-
+    
+    //  -------
+    // | 0 1 2 |
+    //  -------
     auto a = iota(3).diag;
-    auto result = [[0.0, 0.0, 0.0], 
-                   [0.0, 1.0, 0.0], 
-                   [0.0, 0.0, 2.0]].sliced;
+    auto result = [[0, 0, 0], 
+                   [0, 1, 0], 
+                   [0, 0, 2]].sliced;
     assert(a[0] == result[0]);
     assert(a[1] == result[1]);
     assert(a[2] == result[2]);
@@ -213,17 +194,11 @@ unittest
     assert(e.dtype == typeid(double));
     assert(e.dtype != typeid(float));
     assert(e.length == 2);
-    assert(e.size == 2*3*1*3);
+    assert(e.size == (2 * 3 * 1 * 3));
     assert(e.ndim == 4);
     assert(e.strides == [9, 3, 3, 1]);
     assert(e.byteStrides == [72, 24, 24, 8]);
-
-    auto a = iota(3, 4, 5, 6);
-    auto b = a.pack!2;
-    assert(b.ndim == 4);
 }
-
-
 
 /// Shape Manipulation
 unittest
@@ -238,7 +213,6 @@ unittest
        x.squeeze   | x.squeeze, x.unsqueeze
      */
      
-    import std.string : split;
     import mir.ndslice.topology : universal, iota;
     import mir.ndslice.allocation : slice;
     import mir.ndslice.dynamic : transposed;
@@ -246,30 +220,14 @@ unittest
     assert(iota(3, 4).slice.view(-1, 1).shape == [12, 1]);
     assert(iota(3, 4).slice.universal.transposed.view(-1, 6).shape == [2, 6]);
 
-    try {
-        iota(3, 4).slice.view(2, 1);
-    } catch (Exception e) {
-        assert(e.msg.split(":")[0] == "ReshapeError");
-    }
-    try {
-        iota(0).slice.view(2, 1);
-    } catch (Exception e) {
-        assert(e.msg.split(":")[0] == "ReshapeError");
-    }
-
-    assert(iota(2, 3).slice.unsqueeze!0 == [[[0,1,2], [3,4,5]]]);
-    assert(iota(2, 3).slice.unsqueeze!1 == [[[0,1,2]], [[3,4,5]]]);
-    assert(iota(2, 3).slice.unsqueeze!2 == [[[0],[1],[2]], [[3],[4],[5]]]);
-    assert(iota(2, 3).slice.unsqueeze!(-1) == [[[0],[1],[2]], [[3],[4],[5]]]);
+    assert(iota(2, 3).slice.unsqueeze!0 == [[[0, 1, 2],
+                                             [3, 4, 5]]]);
+    assert(iota(2, 3).slice.unsqueeze!1 == [[[0, 1, 2]],
+                                            [[3, 4, 5]]]);
+    assert(iota(2, 3).slice.unsqueeze!2 == [[[0], [1], [2]], 
+                                            [[3], [4], [5]]]);
 
     assert(iota(1, 3, 4).slice.squeeze!0.shape == [3, 4]);
     assert(iota(3, 1, 4).slice.squeeze!1.shape == [3, 4]);
     assert(iota(3, 4, 1).slice.squeeze!(-1).shape == [3, 4]);
 }
-
-
-/// Item selection and manipulation
-
-/// Calculation
-
-/// Arithmetic and comparison operations
